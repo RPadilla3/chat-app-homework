57eb5bfd0c8e014ae971b0d327fadd3dde22f5f9
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-02 14:51:17 -0400

    Created am ajax function that is putting in my login information and storing it.

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -1,4 +1,28 @@
 (function() {
-  'use strict';
-    console.log('It works');
+    'use strict';
+
+    $('.login')
+        .on('submit', function login(event) {
+            event.preventDefault();
+            console.log('username', $('.username').val());
+            $.ajax({
+                    url: '/login',
+                    method: 'POST',
+                    data: JSON.stringify({
+                        username: $('.username').val()
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .done(function handleSuccess(data) {
+                    console.log('it worked', data);
+                })
+                .fail(function handleFailure(xhr) {
+                    console.log('it failed', xhr);
+                })
+        });
+
+
+
 }());
