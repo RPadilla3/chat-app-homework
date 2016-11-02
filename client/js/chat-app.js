c82c1473578e5ddcfb69996afe903c794a26fd2c
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-02 15:43:46 -0400

    Got another function up and running to hold userToken and return it whenever called

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -1,6 +1,36 @@
 (function() {
     'use strict';
 
+    window.chat = window.chat || {};
+    var userToken;
+    var userName;
+
+    window.chat.listenForMessages(function messageHandler(data) {
+
+        $('.send-message')
+            .on('submit', function message(event) {
+                event.preventDefault();
+                $.ajax({
+                        url: '/chat',
+                        method: 'POST',
+                        data: {
+                            message: $('.message').val()
+                        }
+                        headers: {
+                            Authorization: userToken
+                        }
+                    })
+                    .done(function handleYes(success) {
+                        console.log('lit', success);
+                    })
+                    .fail(function handleNo(xhr) {
+                        console.log('nope', xhr);
+                    })
+            })
+
+    });
+
+
     $('.login')
         .on('submit', function login(event) {
             event.preventDefault();
@@ -15,11 +45,12 @@
                         'Content-Type': 'application/json'
                     }
                 })
-                .done(function handleSuccess(data) {
-                    console.log('it worked', data);
+                .done(function handleSuccess(success) {
+                    userToken = success.token;
+                    console.log(userToken);
                 })
                 .fail(function handleFailure(xhr) {
-                    console.log('it failed', xhr);
+                    console.log('It failed', xhr);
                 })
         });
 
